843f8e9c2600be0d1f7b2458140a35ad9c2d577a
v-thoang1 <v-thoang1@SEAHGTHOANG>
2023-03-30 17:01:57 -0700

    Add project files.

---
+++ Exercise Week 1/Program.cs
@@ -0,0 +1,47 @@
+﻿//Console.WriteLine("Enter Value 1 - 10: ");
+//int num = int.Parse(Console.ReadLine());
+//int max = 9;
+
+//if (num == 1)
+//{
+//    Console.WriteLine("Beep!");
+//}
+//else if (num == 2)
+//{
+//    Console.WriteLine("Boop!");
+//}
+//else if (num % 3 == 0)
+//{
+//    Console.WriteLine("I'm sorry, Dave. I'm afraid I can't do that.");
+//}
+//else
+//{
+//    Console.WriteLine("Great! Try again!");
+//}
+
+Console.WriteLine("Enter a Value ");
+int max = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < max; i++)
+
+{
+    if (i == 1)
+    {
+        Console.WriteLine("Beep! ");
+        continue;
+    }
+    if (i == 2)
+    {
+        Console.WriteLine("Boop! ");
+        continue;
+    }
+    if (i % 3 == 0)
+    {
+        Console.WriteLine("I'm sorry, Dave. I'm afraid I can't do that. ");
+        continue;
+    }
+    Console.WriteLine(i);
+
+}
+
+
